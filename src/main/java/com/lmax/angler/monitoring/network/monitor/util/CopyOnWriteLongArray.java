aca4fbe99a3f64bbcf20eda3735201569d963810
pricem <pricem@lmax.com>
2016-05-20 09:01:42 +0100

    delete unused class

--- src/main/java/com/lmax/angler/monitoring/network/monitor/util/CopyOnWriteLongArray.java
+++
@@ -1,8 +0,0 @@
-package com.lmax.angler.monitoring.network.monitor.util;
-
-import java.util.concurrent.atomic.AtomicReference;
-
-public final class CopyOnWriteLongArray
-{
-    private final AtomicReference<long[]> data = new AtomicReference<>(new long[64]);
-}
