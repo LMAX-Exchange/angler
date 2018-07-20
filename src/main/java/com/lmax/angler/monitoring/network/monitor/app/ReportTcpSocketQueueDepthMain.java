547d61f234834e232d4cda608ab235188f84fab2
epickrram <mark@epickrram.com>
2018-07-20 17:09:10 +0100

    Make tcp sample rate configurable

--- src/main/java/com/lmax/angler/monitoring/network/monitor/app/ReportTcpSocketQueueDepthMain.java
+++ src/main/java/com/lmax/angler/monitoring/network/monitor/app/ReportTcpSocketQueueDepthMain.java
@@ -5,15 +5,21 @@ import com.lmax.angler.monitoring.network.monitor.socket.tcp.TcpSocketMonitor;
 import com.lmax.angler.monitoring.network.monitor.socket.tcp.TcpSocketStatisticsHandler;
 
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.HashMap;
+import java.util.Map;
 
 public final class ReportTcpSocketQueueDepthMain implements
         SocketMonitoringLifecycleListener, TcpSocketStatisticsHandler, Runnable
 {
     private static final long SAMPLE_INTERVAL_MS = Long.getLong("angler.tcp.sample.ms", 1000);
     private final TcpSocketMonitor tcpSocketMonitor;
+    private final Map<InetSocketAddress, Long> rxMaxima = new HashMap<>();
+    private final Map<InetSocketAddress, Long> txMaxima = new HashMap<>();
+    private long nextReportTimestamp;
 
     public static void main(String[] args) throws UnknownHostException
     {
@@ -47,8 +53,32 @@ public final class ReportTcpSocketQueueDepthMain implements
                                     final long socketIdentifier, final long inode,
                                     final long receiveQueueDepth, final long transmitQueueDepth)
     {
-        System.out.printf("%d %s:%d; RX: %d, TX: %d%n", TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()),
-                inetAddress, port, receiveQueueDepth, transmitQueueDepth);
+        final InetSocketAddress key = new InetSocketAddress(inetAddress, port);
+        updateMaximum(receiveQueueDepth, rxMaxima, key);
+        updateMaximum(transmitQueueDepth, txMaxima, key);
+
+        if (System.currentTimeMillis() > nextReportTimestamp)
+        {
+            final long epochSeconds = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
+            for (final InetSocketAddress socket : rxMaxima.keySet())
+            {
+                System.out.printf("%d %s; RX: %s, TX: %s%n", epochSeconds,
+                  socket, rxMaxima.get(socket), txMaxima.get(socket));
+            }
+            nextReportTimestamp = System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(1L);
+            rxMaxima.clear();
+            txMaxima.clear();
+        }
+    }
+
+    private void updateMaximum(final long value, final Map<InetSocketAddress, Long> maxima, final InetSocketAddress key)
+    {
+        Long existing = maxima.get(key);
+        if (existing == null)
+        {
+            existing = 0L;
+        }
+        maxima.put(key, Math.max(existing, value));
     }
 
     @Override
